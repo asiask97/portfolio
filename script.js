6de4a7a44654821640fb1d73c4b159e6169b5615
asiask97 <73076054+asiask97@users.noreply.github.com>
2021-07-12 15:27:42 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+document.getElementById('apphtml').addEventListener("click", function() {
+    window.open('https://stock-app-flask-portfolio.herokuapp.com/');
+});
+
+document.getElementById('apphtmlcode').addEventListener("click", function() {
+    window.open('https://github.com/asiask97/stock_app');
+});
+
+
+document.getElementById('matchcode').addEventListener("click", function() {
+    window.open('https://github.com/asiask97/Football-Match-Analysis');
+});
+
+document.getElementById('webcode').addEventListener("click", function() {
+    window.open('https://github.com/asiask97/web-scraper');
+});
